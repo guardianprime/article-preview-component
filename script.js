c82d58c92888007ce102194affbe46cd03b7982c
Guardianprime <okongordian@gmail.com>
2025-01-10 12:04:38 +0100

    added interactivity

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-const test = Document.getElementById("main");
-console.log(main);
+const shareIcon = document.getElementById("share-Icon");
+const wrapperOne = document.getElementById("wrapper-one");
+const wrapperTwo = document.getElementById("wrapper-two");
+const profileDiv = document.getElementById("profile-div");
+
+shareIcon.addEventListener("click", function(){
+    wrapperOne.classList.toggle("hidden");
+    wrapperTwo.classList.toggle("hidden");
+    console.log("toggling classes")
+})
