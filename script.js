635ef3be2fb833290d354ffe9176d07d245614f9
Guardianprime <okongordian@gmail.com>
2025-01-16 13:44:27 +0100

    larger screen popup div implementation

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ const wrapperOne = document.getElementById("wrapper-one");
 const wrapperTwo = document.getElementById("wrapper-two");
 const profileDiv = document.getElementById("profile-div");
 
-shareIcon.addEventListener("click", function(){
-    wrapperOne.classList.toggle("hidden");
-    wrapperTwo.classList.toggle("hidden");
-    profileDiv.classList.toggle("share");
-    console.log("toggling classes")
-})
+shareIcon.addEventListener("click", function () {
+    if (window.screen.width <= 450) {
+        wrapperOne.classList.toggle("hidden");
+        wrapperTwo.classList.toggle("hidden");
+        profileDiv.classList.toggle("share");
+        console.log("toggling classes")
+    } else {
+
+    }
+}) 
